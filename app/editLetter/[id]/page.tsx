0c33a9d9cd705c7b314868b07e2e249ee30b6af0
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-01 14:27:43 +0300

    fix: add generateSaticParams demo

--- app/editLetter/[id]/page.tsx
+++ app/editLetter/[id]/page.tsx
@@ -1,4 +1,12 @@
 import EditLetterForm from "@/components/EditLetterForm"
+import connectMongoDB from "@/libs/mongodb"
+
+export const generateStaticParams = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
 
 export default function EditLetter() {
   return <EditLetterForm />
